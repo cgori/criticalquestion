1c36bd6b23da625cb508b2192002d7619e4d127a
RobH59 <xlrobbielx59@gmail.com>
2019-10-22 17:56:46 +0100

    New pages

---
+++ CriticalQuestion/src/app/pages/account/account.page.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tab1',
+  templateUrl: 'account.page.html',
+  styleUrls: ['account.page.scss']
+})
+export class Tab1Page {
+
+  constructor() {}
+
+}
---
+++ CriticalQuestion/src/app/pages/tabs/tabs.router.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TabsPage } from './tabs.page';
+
+const routes: Routes = [
+  {
+    path: 'tabs',
+    component: TabsPage,
+    children: [
+      {
+        path: 'tab1',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../account/account.module').then(m => m.Tab1PageModule)
+          }
+        ]
+      },
+      {
+        path: 'tab2',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../home/home.module').then(m => m.Tab2PageModule)
+          }
+        ]
+      },
+      {
+        path: 'tab3',
+        children: [
+          {
+            path: '',
+            loadChildren: () =>
+              import('../boardroom/boardroom.module').then(m => m.Tab3PageModule)
+          }
+        ]
+      }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class TabsPageRoutingModule {}
+
